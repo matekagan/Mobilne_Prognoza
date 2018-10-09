eb16ec2ac479e72ddd8257523ebb6ebfd653a16b
Mateusz Kagan <loovlens@gmail.com>
2018-10-09 10:59:26 +0200

    Change textFields into Labels

--- Wheather App/ViewController.swift
+++ Wheather App/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var prevButton: UIButton!
+    
+    @IBOutlet weak var nextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    // MARK: Actions
+    
+    @IBAction func goToNextItem(_ sender: UIButton) {
+    }
+    
+    @IBAction func goToPreviousItem(_ sender: UIButton) {
+    }
 }
 
